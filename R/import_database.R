c5f77597044825960c11a7579f968877e0ad2825
jonathananolan <jonathan.a.nolan@gmail.com>
2024-01-04 10:26:31 -0500

    Added suburb data

--- R/import_database.R
+++ R/import_database.R
@@ -1,16 +1,18 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(grattan)
 
 #Import Victoria's rental database 
 
-rental_file <- list.files(path = "data",full.names = T)
+rental_file_list <- list.files(path = "data",full.names = T)
 
+get_rental_file <- function(rental_file) {
 sheet_names <- excel_sheets(path = rental_file)
 
-import_sheet <- function(x){
+import_sheet <- function(sheet_name){
 # Step 2: Read the first two rows to get the column names
-col_names <- read_excel(rental_file, sheet = x,n_max = 2) 
+col_names <- read_excel(rental_file, sheet = sheet_name,n_max = 2) 
 
 
 # Loop through each row and column of the dataframe
@@ -25,14 +27,14 @@ for (i in 1:nrow(col_names)) {
 
 new_col_names <- paste(col_names[1, -c(1,2)], col_names[2, -c(1,2)], sep = "_")
 
-output <- read_excel(rental_file, sheet = x,skip =1 ) %>% 
-  rename(lga = 2,
-         `Dec 2002` = `Dec 2003...31`, 
-         `Dec 2003` =  `Dec 2003...39`) %>% 
+output <- read_excel(rental_file, sheet = sheet_name,skip =1 ) %>% 
+  rename(area = 2) %>% 
+  rename(any_of(c("Dec 2002" = "Dec 2003...31" ))) %>% 
+  rename(any_of(c("Dec 2003" = "Dec 2003...39" ))) %>% 
   select(-1) %>% 
-  filter(!is.na(lga)) %>% 
+  filter(!is.na(area)) %>% 
   filter(row_number() != 1) %>% 
-  pivot_longer(-lga,
+  pivot_longer(-area,
                names_to = "date_messy",
                values_to = "value") %>%
   mutate(date = if_else(substr(date_messy,1,1)==".",lag(date_messy),date_messy ),
@@ -40,23 +42,51 @@ output <- read_excel(rental_file, sheet = x,skip =1 ) %>%
                          "median",
                          "count" )) %>%
   select(-date_messy) %>% 
-  mutate(series = x)
-
-return(output) }
-
-data <- map_df(sheet_names,import_sheet)  %>% 
+  mutate(file_name = rental_file) %>%
+  mutate(area_type = if_else(str_detect(file_name,"suburb"),"suburb","lga")) %>% 
   filter(value != "-") %>% 
-  mutate(date = dmy(paste0("1 ",date)),
+  mutate(series = sheet_name,
+         date = dmy(paste0("1 ",date)),
          bedrooms = parse_number(series),
          home_type = case_when(str_detect(series,"ouse") ~ "House",
                           str_detect(series,"lat")  ~"Flat",
                           str_detect(series,"All")  ~ "All"),
-         value = parse_number(value))
+         value = parse_number(value),
+         cpi_adjusted_value = if_else(measure == "median",
+                                      value *cpi_inflator(from = date, 
+                                                   to   = today(),
+                                                   series = "original"),
+                                      NA_real_
+                                      )
+         )
 
+return(output)
+
+}
+
+output_sheet <- map_df(sheet_names,import_sheet)
+
+return(output_sheet)
+
+}
+
+
+
+data <- map_df(rental_file_list,get_rental_file)
+
+data %>% write_csv("Victoria's rental index.csv")
 
 data %>% 
-  filter(lga == "Melbourne",
+  filter(area == "Collingwood-Abbotsford",
          measure == "median") %>% 
-  filter(home_type = "Flat") %>% 
+  filter(home_type == "Flat",
+         bedrooms == 2,
+         date > ymd("2013-01-01")) %>% 
   ggplot(aes(x = date, y= value, colour = series))+
-  geom_line()
+  geom_line() +
+  labs(title = "Rents in Collingwood and Abbotsford",
+       caption = "Source - Victoria's rental report",
+       y = element_blank(),
+       x = element_blank())+
+  scale_y_continuous(labels = scales::dollar_format())+
+  theme_minimal()
